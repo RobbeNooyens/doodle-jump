dca6e3541004413498f644133d502860945d3015
Robbe Nooyens <robbenooyens@hotmail.com>
2021-12-02 02:11:39 +0100

    Backup without builderror

--- src/models/PlatformModel.cpp
+++ src/models/PlatformModel.cpp
@@ -5,15 +5,12 @@
 #include "PlatformModel.h"
 
 void models::HorizontalPlatform::update(double elapsed) {
-    PlatformModel::update(elapsed);
 }
 
 void models::TemporaryPlatform::update(double elapsed) {
-    PlatformModel::update(elapsed);
 }
 
 void models::VerticalPlatform::update(double elapsed) {
-    PlatformModel::update(elapsed);
 }
 
 models::PlatformModel::PlatformModel(): EntityModel() {
